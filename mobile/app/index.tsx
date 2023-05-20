27edc361dd42664b60a463f3ed6e30afeaed8b67
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2023-05-20 19:35:05 -0300

    :sparkles: mobile auth integration completed

---
+++ mobile/app/index.tsx
@@ -0,0 +1,120 @@
+import { StatusBar } from 'expo-status-bar'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
+import { styled } from 'nativewind'
+import { useRouter } from 'expo-router'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
+import { useEffect } from 'react'
+
+const StyledStripes = styled(Stripes)
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/c0b6c108f669169d9c2b',
+}
+
+export default function App() {
+  const router = useRouter()
+
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: 'c0b6c108f669169d9c2b',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  async function handleGithubOauthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
+  useEffect(() => {
+    // console.log(
+    //   makeRedirectUri({
+    //     scheme: 'nlwspacetime',
+    //   }),
+    // )
+
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      handleGithubOauthCode(code)
+    }
+  }, [response])
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+  return (
+    <ImageBackground
+      source={blurBg}
+      className=" relative flex-1 items-center bg-gray-900 px-8 py-10"
+      imageStyle={{
+        position: 'absolute',
+        left: '-100%',
+      }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <View className="mb-10 flex-1 items-center justify-center gap-6">
+        <NLWLogo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className=" mtext-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!{' '}
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          onPress={() => signInWithGithub()}
+          activeOpacity={0.7}
+          className="e rounded-full bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  )
+}
---
+++ mobile/src/lib/api.ts
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: 'https://ec7f-168-205-106-227.sa.ngrok.io',
+})
