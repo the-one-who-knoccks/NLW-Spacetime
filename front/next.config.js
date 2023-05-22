4cbf06fd156ce6d66641883d97380eb0061d5094
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2023-05-21 21:01:30 -0300

    :sparkles: memories listing completed

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: [
+      'avatars.githubusercontent.com',
+      'a254-168-205-106-227.sa.ngrok.io',
+      'b5c6-168-205-106-227.sa.ngrok.io',
+      'localhost',
+    ],
   },
 }
 
--- front/src/app/memories/new/page.tsx
+++ front/src/app/memories/new/page.tsx
@@ -3,7 +3,7 @@ import { ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
 export default function NewMemory() {
   return (
-    <div className="flex flex-1 flex-col gap-4">
+    <div className="flex flex-1 flex-col gap-4 p-16">
       <Link
         href="/"
         className="hover:file-gray-1 checkbox-sm flex items-center gap-1 text-gray-200"
